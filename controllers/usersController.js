4a2435cc7f67e6197ef771c6a424d5a5b119657e
chathurperera <chathuraperera007@gmail.com>
2022-10-07 20:52:45 +0530

    user delete controller created

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -52,12 +52,69 @@ const createNewUser = asyncHandler(async (req, res) => {
 // @route PATCH /users
 // @access private
 
-const updateUser = asyncHandler(async (req, res) => {});
+const updateUser = asyncHandler(async (req, res) => {
+  const { id, username, roles, active, password } = req.body;
+
+  //confirm data
+  if (
+    !id ||
+    !username ||
+    !Array.isArray(roles) ||
+    !roles.length ||
+    typeof active !== "boolean"
+  ) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  const user = await User.findById(id).exec();
+  if (!user) {
+    return res.status(400).json({ message: "user not found" });
+  }
+
+  //Allow updates to the original user
+  const duplicate = await User.find({ username }).lean().exec();
+
+  if (duplicate && duplicate._id.toString() !== id) {
+    return res.status(409).json({ message: "Duplicate Username" });
+  }
+
+  user.username = username;
+  user.roles = roles;
+  user.active = active;
+
+  if (password) {
+    user.password = await bcrypt.hash(password, 10);
+  }
+
+  const updatedUser = await user.save();
+  res.status(200).json({ message: `${updatedUser.username} updated` });
+});
 
 // @desc delete user
 // @route DELETE /users
 // @access private
 
-const deleteUser = asyncHandler(async (req, res) => {});
+const deleteUser = asyncHandler(async (req, res) => {
+  const { id } = req.body;
+  if (!id) {
+    return res.status(400).json({ message: "user ID required" });
+  }
+
+  const notes = await Note.findOne({ user: id }).lean().exec();
+
+  if (notes?.length) {
+    return res.status(400).json({ message: "User has assigned notes" });
+  }
+
+  const user = await User.findById(id).exec();
+
+  if (!user) {
+    return res.status(400).json({ message: "User not found" });
+  }
+
+  const result = await User.deleteOne(id);
+
+  res.status(200).json({ message: result });
+});
 
 module.exports = { getAllUsers, createNewUser, updateUser, deleteUser };
